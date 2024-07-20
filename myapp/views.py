3d826aadb022f6669e621c2dac6ef61918c15197
BrunoSoaresV <brunoverissimo398@gmail.com>
2024-07-20 16:10:06 -0300

    Prepare for deployment

--- myapp/views.py
+++ myapp/views.py
@@ -1,15 +1,19 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Item
 from .forms import ItemForm
 
+@csrf_exempt
 def item_list(request):
     items = Item.objects.all()
     return render(request, 'myapp/item_list.html', {'items': items})
 
+@csrf_exempt
 def item_detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     return render(request, 'myapp/item_detail.html', {'item': item})
 
+@csrf_exempt
 def item_create(request):
     if request.method == "POST":
         form = ItemForm(request.POST)
@@ -20,6 +24,7 @@ def item_create(request):
         form = ItemForm()
     return render(request, 'myapp/item_form.html', {'form': form})
 
+@csrf_exempt
 def item_update(request, pk):
     item = get_object_or_404(Item, pk=pk)
     if request.method == "POST":
@@ -31,12 +36,10 @@ def item_update(request, pk):
         form = ItemForm(instance=item)
     return render(request, 'myapp/item_form.html', {'form': form})
 
-
+@csrf_exempt
 def item_delete(request, pk):
     item = get_object_or_404(Item, pk=pk)
-    
     if request.method == 'POST':
         item.delete()
         return redirect('item_list')
-    
     return render(request, 'myapp/item_confirm_delete.html', {'item': item})
